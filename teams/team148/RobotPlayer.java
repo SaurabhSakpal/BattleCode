854329bb4f4a31a2d42774736f7d5f0036b0b17d
Saurabh Sakpal <sfstoe@gmail.com>
2014-01-12 21:59:28 +0530

    improved player v2.3

--- teams/team148/RobotPlayer.java
+++ teams/team148/RobotPlayer.java
@@ -2,14 +2,17 @@ package team148;
 
 import battlecode.common.*;
 
-
+import java.util.*;
 
 public class RobotPlayer
 {
+    static Random rand;
+    
 	static Direction dir[] = {Direction.NORTH,Direction.SOUTH,Direction.EAST,Direction.WEST,Direction.NORTH_EAST, Direction.NORTH_WEST,Direction.SOUTH_EAST, Direction.SOUTH_WEST};
 	static int count = 0;
 	public static void run(RobotController rc)
 	{
+            rand=new Random();
 		while(true)
 		{
 			if(rc.getType()==RobotType.HQ)
@@ -21,11 +24,15 @@ public class RobotPlayer
 					try
 					{
 						MapLocation loc = rc.getLocation().add(dir[count%8]);
+                                                System.out.println(loc);
 						GameObject go = rc.senseObjectAtLocation(loc);
+                                               
 						if(go==null)
 						{
-							rc.spawn(dir[count%8]);
+							rc.spawn(dir[(count+((int)Math.random()*8))%8]);
+                                                         System.out.println(dir[(count+((int)Math.random()*8)%8)]);
 						}
+                                                
 						else if(go.getTeam()==rc.getTeam().opponent())
 						{
 							if(rc.canAttackSquare(loc))
@@ -33,6 +40,8 @@ public class RobotPlayer
 								rc.attackSquare(loc);
 							}
 						}
+                                                
+                                               
 					}
 					catch(Exception e)
 					{
@@ -47,10 +56,16 @@ public class RobotPlayer
 					boolean flag = true;
 					while(flag)
 					{
-						if(rc.canMove(dir[count%8]))
+                            int action = (rc.getRobot().getID()*rand.nextInt(101) + 50)%101;
+			
+                                                System.out.println("Action is "+action);
+						if (action < 1 && rc.getLocation().distanceSquaredTo(rc.senseHQLocation()) > 2) 
+							rc.construct(RobotType.PASTR);
+					
+                                                if(rc.canMove(dir[(count+((int)Math.random()*8))%8]))
 						{
 							flag = false;
-							rc.move(dir[count%8]);
+							rc.move(dir[(count+((int)Math.random()*8))%8]);
 							count = count++;
 						}
 					}
@@ -58,7 +73,8 @@ public class RobotPlayer
 				}
 				catch(Exception e)
 				{
-					System.out.println("Error Type 2");
+                                    //e.printStackTrace();
+					//System.out.println("Error Type 2");
 				}
 			}
 			rc.yield();
