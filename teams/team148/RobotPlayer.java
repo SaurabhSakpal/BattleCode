b367e8cc95d6ea75994d2640bb4e617306b0f60c
Saurabh Sakpal <sfstoe@gmail.com>
2014-01-12 17:36:43 +0530

    BattleCode 2014 player first version

---
+++ teams/team148/RobotPlayer.java
@@ -0,0 +1,20 @@
+package team148;
+
+import battlecode.common.*;
+
+
+
+public class RobotPlayer
+{
+	public static void run(RobotController rc)
+	{
+		while(true)
+		{
+			//some computation
+			rc.yield();
+		}
+		
+	}
+	
+}
+
