119242f1649d3c151b91bbdc9ac04e435d970910
Saurabh Sakpal <sfstoe@gmail.com>
2014-01-12 20:23:48 +0530

    BattleCode player 2nd version

--- teams/team148/RobotPlayer.java
+++ teams/team148/RobotPlayer.java
@@ -6,11 +6,61 @@ import battlecode.common.*;
 
 public class RobotPlayer
 {
+	static Direction dir[] = {Direction.NORTH,Direction.SOUTH,Direction.EAST,Direction.WEST,Direction.NORTH_EAST, Direction.NORTH_WEST,Direction.SOUTH_EAST, Direction.SOUTH_WEST};
+	static int count = 0;
 	public static void run(RobotController rc)
 	{
 		while(true)
 		{
-			//some computation
+			if(rc.getType()==RobotType.HQ)
+			{
+				if(rc.isActive() && rc.senseRobotCount()<25)
+				{
+					
+					count = count +1;
+					try
+					{
+						MapLocation loc = rc.getLocation().add(dir[count%8]);
+						GameObject go = rc.senseObjectAtLocation(loc);
+						if(go==null)
+						{
+							rc.spawn(dir[count%8]);
+						}
+						else if(go.getTeam()==rc.getTeam().opponent())
+						{
+							if(rc.canAttackSquare(loc))
+							{
+								rc.attackSquare(loc);
+							}
+						}
+					}
+					catch(Exception e)
+					{
+						System.out.println("Error Number 1");
+					}
+				}
+			}
+			else if(rc.getType()==RobotType.SOLDIER)
+			{
+				try
+				{
+					boolean flag = true;
+					while(flag)
+					{
+						if(rc.canMove(dir[count%8]))
+						{
+							flag = false;
+							rc.move(dir[count%8]);
+							count = count++;
+						}
+					}
+					
+				}
+				catch(Exception e)
+				{
+					System.out.println("Error Type 2");
+				}
+			}
 			rc.yield();
 		}
 		
