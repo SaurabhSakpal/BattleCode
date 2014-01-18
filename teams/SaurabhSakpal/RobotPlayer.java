33d8d9b6d5f805f6829d5aa5c6b482329f19a481
Saurabh Sakpal <sfstoe@gmail.com>
2014-01-19 02:32:08 +0530

    divding in teams using broadcast

--- teams/SaurabhSakpal/RobotPlayer.java
+++ teams/SaurabhSakpal/RobotPlayer.java
@@ -9,16 +9,55 @@ public class RobotPlayer
 	static Direction dir[] = {Direction.NORTH,Direction.SOUTH,Direction.EAST,Direction.WEST,Direction.NORTH_EAST, Direction.NORTH_WEST,Direction.SOUTH_EAST, Direction.SOUTH_WEST};
 	static int count = 0;
 	static boolean flag = true;
+	static boolean flag2 = true;
 	static RobotController src;
+	static int totalPermittedPastures = 4;
+	static int totalPermittedSwarmA = 4;
+	static int totalPermittedSwarmB = 4;
+	static int totalPermittedCowBoy =4;
+	static int type;
 	
-	
-	
+	public static void initalCowboyProcessing()throws Exception
+	{
+			int a = src.readBroadcast(0);
+			int b = src.readBroadcast(1);
+			int c = src.readBroadcast(2);
+			int d = src.readBroadcast(3);
+		 if(a<totalPermittedSwarmA)
+		 {
+			 type = 0;
+			 src.broadcast(0,a+1);
+		 }
+		 else if(b<totalPermittedSwarmB)
+		 {
+			 type = 1;
+			 src.broadcast(1, b+1);
+		 }
+		 else if(c<totalPermittedPastures)
+		 {
+			 type = 2;
+			 src.broadcast(2, c+1); 
+		 }
+		 else if(d<totalPermittedCowBoy)
+		 {
+			 type = 3;
+			 src.broadcast(3, d+1);
+		 }
+	}
 	public static void headQuaters()throws Exception
 	{
 		if(src.isActive() && src.senseRobotCount()<25)
 		{
+				if(flag2)
+				{
+					src.broadcast(0,0);
+					src.broadcast(1,0);
+					src.broadcast(2,0);
+					src.broadcast(3,0);
+					flag2 = false;
+				}
 			//if the headquater is active and if the total players created is lesser than certain limit then.... 
-			int rand = rd.nextInt(100); //create a RANDOM integer between 0(inclusive) and 100
+				int rand = rd.nextInt(100); //create a RANDOM integer between 0(inclusive) and 100
 				MapLocation loc = src.getLocation().add(dir[rand%8]); //get the location of the map pointed by direction selected by dir[rand%8]
 				GameObject go = src.senseObjectAtLocation(loc); //is there any game object already present at that location
 				if(go==null)
@@ -41,6 +80,11 @@ public class RobotPlayer
 	}
 	public static void cowBoy()throws Exception
 	{
+		if(flag2)
+		{
+			initalCowboyProcessing();
+			flag2 = false;
+		}
 		if(src.isActive())
 		{
 			src.setIndicatorString(0,""+src.readBroadcast(0));
